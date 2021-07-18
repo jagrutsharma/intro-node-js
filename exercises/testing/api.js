82951557765f15429aef3646b344bd0818954c02
Jagrut Sharma <jagrutsharma@gmail.com>
2021-07-18 16:12:14 -0700

    Completed exercise 5 (adding tests for apis via jest) - debugger entry

--- exercises/testing/api.js
+++ exercises/testing/api.js
@@ -9,16 +9,28 @@ app.use(urlencoded({extended: true}))
 app.use(json())
 
 app.get('/user/:id', async (req, res) => {
-  const id = req.id
-  // should ge user by given id in route param
-  const user = await users.findUser(user => user.id === id)
-  res.status(200).send(user)
+	// The below is what will make Chrome open up this file in inspect mode
+	// debugger
+	const id = parseInt(req.params.id)
+	// should get user by given id in route param
+  try {
+	const user = await users.findUser(id)
+	res.status(200).send(user)
+  }
+  catch (error) {
+  	res.status(404).send(`User with id ${id} not found`)
+  }
 })
 
 app.delete('/user/:id', async (req, res) => {
-  const id = req.id
-  await users.deleteUser(id)
-  res.status(201).send({id})
+  const id = parseInt(req.params.id)
+	try {
+		await users.deleteUser(id)
+		res.status(201).send({id})
+	}
+	catch(error) {
+		res.status(404).send(`User with id ${id} not found`)
+	}
 })
 
 module.exports = app
--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,47 @@
+const {findUser, deleteUser} = require('./users')
+const assert = require('chai').assert
+// expect is from jest, do not import from chai.
+
 // write some tests
-describe('users', () => {
-  
+describe('users_test', () => {
+	describe('users_test - find user', () => {
+		it('find user - valid id', async () => {
+			try {
+				const res = await findUser(1)
+				expect(res.id).toBe(1)
+				expect(res.email).toBe('readycoder1@gmail.com')
+			} catch (error) {
+				assert(false, 'exception should not be thrown while trying to find user 1')
+			}
+		})
+
+		it('find user - invalid id', async () => {
+			try {
+				await findUser(100)
+				assert(false, 'exception should be thrown for invalid user');
+			} catch (error) {
+				assert(error.message === 'No user with id \"100\"')
+			}
+		})
+	})
+
+	describe('users_test - delete user', () => {
+		it('delete user - valid id', async () => {
+			try {
+				const res = await deleteUser(1)
+				expect(res.id).toBe(1)
+			} catch (error) {
+				assert(false, 'exception should not be thrown while trying to delete user 1')
+			}
+		})
+
+		it('delete user - invalid id', async () => {
+			try {
+				await deleteUser(100)
+				assert(false, 'exception should be thrown for trying to delete invalid user');
+			} catch (error) {
+				assert(error.message === 'No user with id \"100\"')
+			}
+		})
+	})
 })
